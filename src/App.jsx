56d7cd62242655177f37f59d5617b411b59347c9
Juan Serrano <juanignacioserranoluna@gmail.com>
2023-02-13 13:39:33 -0300

    updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log('useEffect', { enabled });
+
+    const handlMove = (e) => {
+      const { clientX, clientY } = e;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener('pointermove', handlMove);
+    }
+
+    //clean useEffect
+    return () => {
+      window.removeEventListener('pointermove', handlMove);
+    };
+  }, [enabled]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          top: position.y,
+          left: position.x,
+          width: 30,
+          height: 30,
+          backgroundColor: 'black',
+          borderRadius: '50%',
+          border: '1px solid #ccc',
+          transform: `translate(${position.x}px, ${position.y}px})`,
+          display: enabled ? 'block' : 'none',
+          // inser delay to show the cursor
+          transition: 'all 0.05s ease',
+        }}
+      />
+      <h2>Mouse Follower</h2>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} Seguir mi puntero
+      </button>
+    </main>
+  );
 }
-
-export default App
